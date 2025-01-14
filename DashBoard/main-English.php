0f3318146193b7d19d9926e3031bd8d2f8cccc4b
dd00bb-id <dnsl0630@naver.com>
2025-01-15 00:27:29 +0900

    Update main-English.php

--- DashBoard/main-English.php
+++ DashBoard/main-English.php
@@ -11,7 +11,7 @@ $userName = isset($_SESSION['userName']) ? $_SESSION['userName'] : '이름없음
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dongui University Web Server Team Project</title>
+    <title>University Web Server Team Project</title>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" href="styles.css">
 
